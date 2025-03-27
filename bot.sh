683b9852721f85275467f8c1fe44f790235ddbcf
dfragments <86169011+dfragments@users.noreply.github.com>
2025-03-27 14:11:07 +0100

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 while true; do
+    # Find and terminate any existing 'npm run start' process
+    pkill -f "npm run start"
+    
+    # Start the process
     npm run start
-    sleep 43200  # 12 hours in seconds
+    
+    # Wait for 6 hours before restarting
+    sleep 21600  # 6 hours in seconds
 done
-
